5ec51bf7cd71f8c9dd17bd2ad2aa9743d23b03d2
parkjw <woojang812@gmail.com>
2024-01-10 17:21:56 +0900

    SecurityConfig 파일 일부 수정

--- src/main/java/socketTest/socketTestspring/service/JwtTokenUtil.java
+++ src/main/java/socketTest/socketTestspring/service/JwtTokenUtil.java
@@ -12,12 +12,12 @@ public class JwtTokenUtil {
     public static String createToken(String memberId, String key, long expireTimeMs){
         Claims claims = Jwts.claims();
         claims.put("memberId",memberId);
-        Key encodedKey = Keys.secretKeyFor(SignatureAlgorithm.HS256); //해싱 알고리즘으로 입력받은 키를 암호화
+        Key encodedKey = Keys.secretKeyFor(SignatureAlgorithm.HS256); //해싱 알고리즘으로 입력 받은 키를 암호화
 
         return Jwts.builder()
                 .setClaims(claims)
-                .setIssuedAt(new Date(System.currentTimeMillis()))//현재시간
-                .setExpiration(new Date(System.currentTimeMillis() + expireTimeMs))//현재시간 + 종료시간 = 만료 시간
+                .setIssuedAt(new Date(System.currentTimeMillis()))//현재 시간
+                .setExpiration(new Date(System.currentTimeMillis() + expireTimeMs))//현재 시간 + 종료 시간 = 만료 시간
                 .signWith(encodedKey)
                 .compact();
     }
