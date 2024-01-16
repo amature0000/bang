b2a8894077531c76b9e54bf7e660e95b35006e11
amature0000 <amature0000@naver.com>
2024-01-16 19:05:35 +0900

    Exception 발생상황에 따라 적절한 클래스로 변경

--- src/main/java/socketTest/socketTestspring/filter/JwtFilter.java
+++ src/main/java/socketTest/socketTestspring/filter/JwtFilter.java
@@ -1,6 +1,5 @@
 package socketTest.socketTestspring.filter;
 
-import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
