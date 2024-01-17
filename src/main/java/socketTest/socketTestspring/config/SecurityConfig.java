523d02be6e3028041bb0b2abe13b932e0737614f
parkjw <woojang812@gmail.com>
2024-01-17 18:52:53 +0900

    SecurityConfig 인가 내용 추가

--- src/main/java/socketTest/socketTestspring/config/SecurityConfig.java
+++ src/main/java/socketTest/socketTestspring/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,7 +36,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(request -> request
                         .requestMatchers(PathRequest.toH2Console()).permitAll()
                         .requestMatchers("api/users/login","api/users/join").permitAll()
-                        .requestMatchers("api/**").authenticated()
+                        .requestMatchers(HttpMethod.POST,"api/**").authenticated() //api 경로의 post 방식은 인가 필요
                         .requestMatchers("ws").authenticated() //stomp handshake
                 ) //TODO : authenticated() denied 시 아무 일도 일어나지 않음. 클라이언트에게 알림을 보내야 할 듯?
 
