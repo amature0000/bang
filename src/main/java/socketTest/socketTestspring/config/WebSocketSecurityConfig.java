6f38b05e04ddae52bc56f5fde80e9decbe375374
amature0000 <amature0000@naver.com>
2024-01-20 00:00:47 +0900

    미구현 사항 주석으로 추가

--- src/main/java/socketTest/socketTestspring/config/WebSocketSecurityConfig.java
+++ src/main/java/socketTest/socketTestspring/config/WebSocketSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.socket.AbstractSecurit
 
 // 참고 : https://docs.spring.io/spring-security/reference/servlet/integrations/websocket.html
 // CSRF disable 을 위해서 deprecated 된 버전을 사용해야 함(최신버전 에서는 지원하지 않음)
+// TODO : deny 시 http status 200이 뜨는 것인지 테스트 툴에서는 success 로 찍힌다. 현재 deprecated 버전을 사용 중이기 때문에 인터넷에서 예시 확인하고 수정하면 될 듯.
 @Configuration
 public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
 
