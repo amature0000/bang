a6ddb4c9f1b3bcee7550ea5504537456f9b4bb90
amature0000 <amature0000@naver.com>
2024-01-24 20:40:06 +0900

    오해한 내용 삭제

--- src/main/java/socketTest/socketTestspring/service/MemberService.java
+++ src/main/java/socketTest/socketTestspring/service/MemberService.java
@@ -49,9 +49,7 @@ public class MemberService {
 
         TokenDto tokenDto = jwtTokenUtil.createTokens(memberId);
         //refreshToken 있는지 확인
-        // TODO : 어차피 토큰이 있든 없든 새 토큰을 할당하는데, if-else 로 구분해야 할 이유가 없음. 또한 Optional 객체를 활용하지 않아 발생 가능한 예외들이 무시됨.
         // TODO : Refresh Token 을 재할당 하는 로직은 MemberService 의 login 메소드가 아닌 다른 곳에서 구현해야 적절함. 유저 정보 관리 Service 에 구현해야 하는 내용이 아님.
-        // TODO : save 명령만 하고 있는데, 기존의 Token 데이터는 계속 쌓이기만 하는 건지? (위의 내용과 더불어) 데이터 처리 및 발생 가능한 에러에 대한 핸들링이 미흡함.
         Optional<RefreshToken> refreshToken = refreshTokenRepository.findByMemberId(memberId);
         if(refreshToken.isPresent()){
             refreshTokenRepository.save(refreshToken.get().updateToken(tokenDto.refreshToken()));
