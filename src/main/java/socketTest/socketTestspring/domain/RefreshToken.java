ebe88fdbf6a33ef92378b207c5684eede147f170
amature0000 <amature0000@naver.com>
2024-01-25 19:50:32 +0900

    RefreshToken.memberId unique 로 설정

--- src/main/java/socketTest/socketTestspring/domain/RefreshToken.java
+++ src/main/java/socketTest/socketTestspring/domain/RefreshToken.java
@@ -1,9 +1,6 @@
 package socketTest.socketTestspring.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,7 +16,9 @@ public class RefreshToken {
 
     @NotBlank
     private String refreshToken;
+
     @NotBlank
+    @Column(unique = true)
     private String memberId;
 
     public RefreshToken(String token, String id){
